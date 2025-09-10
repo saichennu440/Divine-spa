736be92e02ac9539476e30abcf9a17440e59f56e
mesaichennu <chennusaiprasad24@gmail.com>
2025-09-11 00:12:21 +0530

    changed something

--- api/admin/reviews.ts
+++ api/admin/reviews.ts
@@ -17,9 +17,9 @@ export default async function handler(req: any, res: any) {
   // Basic CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, application/json');
 
-  if (req.method === 'OPTIONS') return res.status(200).end();
+  //if (req.method === 'OPTIONS') return res.status(200).end();
 
   try {
     // Auth check early

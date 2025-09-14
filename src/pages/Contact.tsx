39c4ea66a8285803849a7eabf7206d5ebcb53026
mesaichennu <chennusaiprasad24@gmail.com>
2025-09-15 01:33:10 +0530

    contact page spacing

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -121,7 +121,7 @@ const Contact: React.FC = () => {
       </section>
 
       {/* Contact Form & Info */}
-    <section className="py-20">
+    <section className="py-5">
            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
              <div className="grid grid-cols-1 lg:grid-cols-2 gap-16">
                {/* Contact Form */}
